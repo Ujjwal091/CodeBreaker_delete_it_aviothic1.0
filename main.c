df8f788029a974a770666ba35f5e55b029ab76a3
Ujjwal jain <72179088+Ujjwal091@users.noreply.github.com>
2023-05-29 19:59:54 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,51 @@
 #include <stdio.h>
 
+// taking command line arguments
 int main(int argc, char *argv[]) {
 
+    // if no file name is provided by the user
     if (argc == 1){
         printf("Please proved at least one file to be deleted\n");
         return  1;
     }
-  for(int i = 1; i<argc; i++){
-      FILE *fp = fopen(argv[i], "r");
-      if(!fp){
+
+    // traverse through all the file name provided by the user
+    for(int i = 1; i<argc; i++){
+
+        // to check whether this file is exist or not on the system
+        // if it is not there then check next file
+        FILE *fp = fopen(argv[i], "r");
+        if(!fp){
+            printf("Can not open file %s\n", argv[i]);
+            continue;
+        }
+        fclose(fp);
+
+        // open file in write mode to first override the data
+        FILE * file = fopen(argv[i], "w");
+        if(!file){
           printf("Can not open file %s\n", argv[i]);
-          continue;
-      }
-      fclose(fp);
-    FILE * file = fopen(argv[i], "w");
+            continue;
+        }
 
-    if(!file){
-      printf("Can not open file %s\n", argv[i]);
-        continue;
+        // override the previous data
+        int ch;
+        while(1){
+            ch = fgetc(file);
+            if(ch == EOF){
+               break;
+            }
+            else{
+            fputc(1, file);
+        }
     }
-
-    int ch;
-
-    while(1){
-      ch = fgetc(file);
-      if(ch == EOF){
-        break;
-      }
-      else{
-      fputc(1, file);
-      }
-    }
-
     fclose(file);
+
+    // deleting the reference in order to free up the memory
     if (remove(argv[i]) == 0)
       printf("%30s Deleted successfully\n", argv[i]);
     else
       printf("Unable to delete the file %30s\n", argv[i]);
   }
-
   return 0;
 }
